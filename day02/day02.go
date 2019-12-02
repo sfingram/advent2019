e70f4ed9381f200d22716e0618b7f39d58e3f4e3
Stephen Ingram <SIngram@flexera.com>
2019-12-02 08:04:00 -0500

    day 2

---
+++ day02/day02.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+const exitError = 1
+const goalState = 19690720
+const gridSize = 100
+
+// Define a split function that separates on commas. (stolen from https://golang.org/src/bufio/example_test.go)
+func commaSplit(data []byte, atEOF bool) (advance int, token []byte, err error) {
+	for i := 0; i < len(data); i++ {
+		if data[i] == ',' {
+			return i + 1, data[:i], nil
+		}
+	}
+	if !atEOF {
+		return 0, nil, nil
+	}
+	// There is one final token to be delivered, which may be the empty string.
+	// Returning bufio.ErrFinalToken here tells Scan there are no more tokens after this
+	// but does not trigger an error to be returned from Scan itself.
+	return 0, data, bufio.ErrFinalToken
+}
+
+func loadProgram(filename string) []int {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("Error opening file %s", filename)
+	}
+	defer file.Close()
+
+	data := make([]int, 0, 1<<10)
+	scanner := bufio.NewScanner(file)
+	scanner.Split(commaSplit)
+	for scanner.Scan() {
+		val, _ := strconv.Atoi(scanner.Text())
+		data = append(data, val)
+	}
+
+	if err := scanner.Err(); err != nil {
+		os.Exit(exitError)
+	}
+
+	return data
+}
+
+func fixProgram(noun int, verb int, programData []int) []int {
+	programData[1] = noun
+	programData[2] = verb
+	return programData
+}
+
+func copyProgram(programData []int) []int {
+	copy := make([]int, len(programData))
+	for i, v := range programData {
+		copy[i] = v
+	}
+	return copy
+}
+
+func executeProgram(programData []int) []int {
+
+	for i := 0; i < len(programData); i++ {
+		switch programData[i] {
+		case 1:
+			// log.Printf("%03d : ADD %03d %03d %03d", i, programData[i+1], programData[i+2], programData[i+3])
+			programData[programData[i+3]] = programData[programData[i+1]] + programData[programData[i+2]]
+			i += 3
+		case 2:
+			// log.Printf("%03d : MUL %03d %03d %03d", i, programData[i+1], programData[i+2], programData[i+3])
+			programData[programData[i+3]] = programData[programData[i+1]] * programData[programData[i+2]]
+			i += 3
+		case 99:
+			// log.Printf("%03d : EXIT", i)
+			return programData
+		default:
+			log.Printf("Error token at %d: %d", i, programData[i])
+		}
+	}
+	return programData
+}
+
+func main() {
+
+	if len(os.Args) < 2 {
+		os.Exit(exitError)
+	}
+	filename := os.Args[1]
+	noun := 12
+	verb := 2
+	programData := fixProgram(noun, verb, loadProgram(filename))
+	originalProgram := copyProgram(programData)
+	programData = executeProgram(programData)
+
+	fmt.Printf("Part 1: %d\n", programData[0])
+
+	// Grid search for answer
+
+	for i := 0; i < gridSize; i++ {
+		for j := 0; j < gridSize; j++ {
+
+			noun = i
+			verb = j
+			programData = copyProgram(originalProgram)
+
+			fixProgram(noun, verb, programData)
+			programData = executeProgram(programData)
+
+			if programData[0] == goalState {
+				fmt.Printf("Part 2: %d \n", 100*noun+verb)
+				os.Exit(0)
+			}
+		}
+	}
+}
